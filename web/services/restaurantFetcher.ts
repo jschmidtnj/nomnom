ab2704cddeec780f384d7a060c2c58b233c2a120
Joshua Schmidt <jschmidtnj@gmail.com>
2026-02-01 04:46:46 +0000

    fix map

--- web/services/restaurantFetcher.ts
+++ web/services/restaurantFetcher.ts
@@ -1,7 +1,7 @@
 
 import { Restaurant, Coordinates } from "../types";
 
-const isLocal = true;
+const isLocal = false;
 
 export const fetchRecommendedRestaurants = async (_coords: Coordinates): Promise<{ restaurants: Restaurant[], rawText: string }> => {
   try {
